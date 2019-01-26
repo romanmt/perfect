2056fdd8d5d1619afc31ce2c39cf27b6a85c2fe0
Matt Roman <romanmt@gmail.com>
2019-01-25 22:53:29 -0500

    added material ui

--- ui/pages/changes.jsx
+++ ui/pages/changes.jsx
@@ -3,7 +3,15 @@ import PropTypes from 'prop-types'
 import { initStore } from '../store'
 import withRedux from 'next-redux-wrapper'
 import { kea } from 'kea'
-const { List } = require('immutable');
+import { List } from 'immutable'
+import { withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 
 @kea({
     actions: () => ({
@@ -17,31 +25,60 @@ const { List } = require('immutable');
 })
 
 class Changes extends Component {
+
     render() {
-        const { combinations } = this.props
+        const { classes, combinations } = this.props
         const { addCombination } = this.actions
         return <div>
-            <table>
-                <thead>
-                    <th>A Chord</th>
-                    <th>B Chord</th>
-                    <th>BPM</th>
-                    <th>Last Practiced</th>
-                </thead>
-                <tbody>
-                    {combinations.map(function(combination){
-                         return <tr>
-                             <td>{combination[0]}</td>
-                             <td>{combination[1]}</td>
-                             <td>{combination[2]}</td>
-                             <td>{combination[3]}</td>
-                         </tr>
-                     })}
-                </tbody>
-            </table> 
-            <button onClick={() => {addCombination(["Amaj", "CMaj", 0, "yesterday"])}}>Add Combination</button>
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>A Chord</TableCell>
+                            <TableCell>B Chord</TableCell>
+                            <TableCell>BPM</TableCell>
+                            <TableCell>Last Practiced</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {combinations.map(function(combination){
+                            return <TableRow>
+                                <TableCell>{combination[0]}</TableCell>
+                                <TableCell>{combination[1]}</TableCell>
+                                <TableCell>{combination[2]}</TableCell>
+                                <TableCell>{combination[3]}</TableCell>
+                            </TableRow>
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
+
+            <Button variant="outlined"
+                    className={classes.button}
+                    onClick={() => {addCombination(["Amaj", "CMaj", 0, "yesterday"])}}>
+                Add Combination
+            </Button>
         </div>
     }
 }
 
-export default withRedux(initStore)(Changes)
+const styles = theme => ({
+    root: {
+        width: '80%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+        margin: '30px',
+    },
+    table: {
+        minWidth: 700,
+    },
+    button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none',
+    }
+})
+
+const styled = withStyles(styles) (Changes)
+export default withRedux(initStore) (styled)
